6b9783f8166447620e5ac527af377e0dad7dcac6
caleisach <caleb.churata@utec.edu.pe>
2022-12-08 11:40:21 -0500

    asdf

---
+++ bot_para_llenar_form.py
@@ -0,0 +1,73 @@
+'''debeloped by: CaleIsaCh'''
+# este bot llena un formulario de una pagina web de google
+# importar la libreria de selenium para poder usar el webdriver
+from selenium import webdriver
+# importar la libreria de time para poder usar el sleep
+import time
+# importar la libreria de os para poder usar el system
+import os
+# importar la libreria de sys para poder usar el argv
+import sys
+# importar la libreria de random para poder usar el randint
+import random
+# importar la libreria de string para poder usar el ascii_letters
+import string
+# importar la libreria de pyautogui para poder usar el click
+import pyautogui 
+
+# el enlace del formulario es:
+# https://forms.gle/mSGRRrMJkHiXmw4j6
+
+# definir la funcion de bot
+def bot():
+    # definir la variable de driver como un webdriver de chrome
+    driver = webdriver.Chrome()
+    # abrir el navegador con el enlace del formulario
+    driver.get("https://forms.gle/mSGRRrMJkHiXmw4j6")
+    # esperar 5 segundos
+    time.sleep(5)
+    # hacer click en el boton de la opcion de contestar el formulario
+    pyautogui.click(347, 110)
+    # esperar 2 segundos
+    time.sleep(2)
+    # hacer click en el boton de la opcion de contestar el formulario
+    pyautogui.click(347, 110)
+    # esperar 2 segundos
+    time.sleep(2)
+    # hacer click en el boton de la opcion de contestar el formulario
+    pyautogui.click(347, 110)
+    # esperar 2 segundos
+    time.sleep(2)
+    # hacer click en el boton de la opcion de contestar el formulario
+    pyautogui.click(347, 110)
+    # esperar 2 segundos
+    time.sleep(2)
+    # hacer click en el boton de la opcion de contestar el formulario
+    pyautogui.click(347, 110)
+    # esperar 2 segundos
+    time.sleep(2)
+    # hacer click en el boton de la opcion de contestar el formulario
+    pyautogui.click(347, 110)
+    # esperar 2 segundos
+    time.sleep(2)
+    # hacer click en el boton de la opcion de contestar el formulario
+    pyautogui.click(347, 110)
+    # esperar 2 segundos
+    time.sleep(2)
+    # hacer click en el boton de la opcion de contestar el formulario
+    pyautogui.click(347, 110)
+    # esperar 2 segundos
+    time.sleep(2)
+    # hacer click en el boton de la opcion de contestar el formulario
+    pyautogui.click(347, 110)
+    # esperar 2 segundos
+    time.sleep(2)
+    # hacer click en el boton de la opcion de contestar el formulario
+    pyautogui.click(347, 110)
+    # esperar 2 segundos
+    time.sleep(2)
+    # hacer click en el boton de la opcion de contestar el formulario
+    pyautogui.click
+    # esperar 2 segundos
+    
+bot()
